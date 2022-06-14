25a5c169c18b74407b1bafb889befb99b79a75a0
Derian2426 <victor.chun2017@uteq.edu.ec>
2022-06-13 20:38:07 -0500

    API

--- app/src/main/java/com/example/myapplicationapivolley/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationapivolley/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.myapplicationapivolley
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
+import android.view.View
+import android.widget.TextView
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun eventEnviar(view:View){
+        val textView = findViewById<TextView>(R.id.txtText)
+        val cola = Volley.newRequestQueue(this)
+        val url = "https://gorest.co.in/public/v1/users"
+
+        // Request a string response from the provided URL.
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            Response.Listener<String> { response ->
+                textView.text = "Resultado----> ${response.toString()}"
+            },
+            Response.ErrorListener { textView.text = "Ocurrio un error!" })
+        textView.movementMethod=ScrollingMovementMethod()
+        cola.add(stringRequest)
+
     }
 }
